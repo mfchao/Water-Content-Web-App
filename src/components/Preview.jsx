774397f3ed68e8ec0b80dbf1ce5219847178ec9e
Maggie Chao <maggie.chao@samsung.com>
2025-08-15 19:44:34 -0700

    url

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -14,7 +14,7 @@ function Preview() {
     if (window.location.hostname === "localhost") {
       return "http://127.0.0.1:5000";
     } else {
-      return "https://water-content-web-app.vercel.app/api";
+      return "https://water-content-web-app-maggie-chaos-projects.vercel.app/api";
     }
   }
 
